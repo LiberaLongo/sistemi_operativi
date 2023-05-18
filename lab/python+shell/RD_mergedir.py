e2e443a2f2e02c4871e20514cfcdd1bdfc0d483c
LiberaLongo <liberalongo@gmail.com>
2023-05-18 16:31:31 +0200

    mergedir del prof, notare l'uso di os.walk per python e di find per shell

---
+++ lab/python+shell/RD_mergedir.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+def copyapp(s, d):
+  with open(s, 'r') as sf:
+    with open(d, 'a') as df:
+      df.write(sf.read())
+
+def copyappdir(sd, dd):
+  for d,_,fx in os.walk(sd):
+    dstdir = dd + '/' + d[len(sd):]
+    os.makedirs(dstdir, exist_ok=True)
+    for f in fx:
+      copyapp(sd + '/' + f, dstdir + '/' + f)
+
+if __name__ == "__main__":
+  copyappdir(sys.argv[1], sys.argv[3])
+  copyappdir(sys.argv[2], sys.argv[3])
---
+++ lab/python+shell/RD_mergedir.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+copyapp() {
+  for f in `cd $1 ; find . -type f`
+  do
+    mkdir -p `dirname $2/$f`
+    cat $1/$f >> $2/$f
+  done
+}
+
+mkdir -p $3
+copyapp $1 $3
+copyapp $2 $3
